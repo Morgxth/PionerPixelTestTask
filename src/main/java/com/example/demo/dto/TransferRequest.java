95780ea135cf096a14c889a5de24b95241c56f1f
Said Goygov <goygovs@gmail.com>
2025-05-12 19:53:31 +0300

    added constructor

--- src/main/java/com/example/demo/dto/TransferRequest.java
+++ src/main/java/com/example/demo/dto/TransferRequest.java
@@ -2,11 +2,13 @@ package com.example.demo.dto;
 
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@AllArgsConstructor
 public class TransferRequest {
     @NotNull
     private Long toUserId;
